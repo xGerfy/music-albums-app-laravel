9bc9bda407f9db0ebe89432fe58358d29c6a6485
Roman Ponomarev <xgerfy@gmail.com>
2026-04-05 20:13:11 +0400

    add: albums, albums log models

--- src/app/Models/AlbumLog.php
+++ src/app/Models/AlbumLog.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AlbumLog extends Model
 {
-    //
+    protected $fillable = [
+      'album_id',
+      'user_id',
+      'action',
+      'old_values',
+      'new_values',
+    ];
+
+    protected $casts = [
+        'old_values' => 'array',
+        'new_values' => 'array',
+        'created_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
